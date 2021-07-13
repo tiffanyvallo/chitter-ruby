c0d59596be0aa73e7702a91e77a486e9f345dff9
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-13 02:29:03 +0100

    failing test user.create method

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -15,11 +15,14 @@ describe User do
   end
      
 
-  # context '#create' do
-  #   it 'can create a user and add it to the db' do
-  #     User.create(username: 'test1', email: 'test@email.com', password: 'testpassword1')
-
-  #   end
-  # end
+  context '#create' do
+    it 'can create a user and add it to the db' do
+      User.create('test1', 'test@email.com', 'testpassword1')
+      user = User.all.first
+      expect(user.username).to eq 'test1'
+      expect(user.email).to eq 'test@email.com'
+      expect(user.password).to eq 'testpassword1'
+    end
+  end
 
 end
