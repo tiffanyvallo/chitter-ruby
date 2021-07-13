46ab643204271dfc9bb9248c7aa9277835f77826
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-13 02:35:24 +0100

    user.create method passes

--- lib/user.rb
+++ lib/user.rb
@@ -22,4 +22,16 @@ class User
       User.new(username: user['username'], email: user['email'], password: user['password'])
     end
   end
+
+  def self.create(username, email, password)
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'rubychitter_test')
+    else 
+      connection = PG.connect(dbname: 'rubychitter')
+    end
+
+    result = connection.exec("INSERT INTO users(username, email, password) VALUES('#{username}', '#{email}', '#{password}') RETURNING username, email, password;")
+    User.new(username: result[0]['username'], email: result[0]['email'], password: result[0]['password'])
+  end
+
 end
