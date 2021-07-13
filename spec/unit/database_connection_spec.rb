1b1ddb84d334fd941e9edf5c23bdf5d5e0d9be39
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-13 03:02:37 +0100

    failing test Database connection.setup method

--- spec/unit/database_connection_spec.rb
+++ spec/unit/database_connection_spec.rb
@@ -2,4 +2,11 @@ require 'database_connection'
 
 describe DatabaseConnection do
   
+  context '#.setup' do
+    it 'can set up the db' do
+      expect(PG).to receive(:connect).with(dbname: 'rubychitter_test')
+      DatabaseConnection.setup('rubychitter_test') 
+    end
+  end
+  
 end
