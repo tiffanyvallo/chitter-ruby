9d53d4c010c207f9c81c527f50674de4ff4883d4
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-19 22:11:27 +0100

    failing test initiliaze method added for peep class

--- spec/unit/peep_spec.rb
+++ spec/unit/peep_spec.rb
@@ -2,5 +2,12 @@ require 'peep'
 require 'pg'
 
 describe Peep do
- 
+  let(:peep) { double(:peep, peep_id: 1, message: 'Test peep', timestamp: Date.today )}
+  context '#initialize' do
+    it 'should initialize with set attributes' do
+      expect(peep.peep_id).to eq(1)
+      expect(peep.message).to eq('Test peep')
+      expect(peep.timestamp).to eq("#{Date.today}")
+    end
+  end
 end
