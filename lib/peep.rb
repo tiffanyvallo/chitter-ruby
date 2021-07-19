ba9b7b5d3cdaa82b5d5d5e6ff8651fb61f208481
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-19 22:17:59 +0100

    pass test initialize method in peep class passing

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,3 +1,9 @@
 class Peep 
 
+  def initialize(peep_id:, messgae:, timestamp:)
+    @peep_id = peep_id
+    @message = message
+    @timestamp = timestamp
+  end
+  
 end
--- spec/unit/peep_spec.rb
+++ spec/unit/peep_spec.rb
@@ -2,12 +2,12 @@ require 'peep'
 require 'pg'
 
 describe Peep do
-  let(:peep) { double(:peep, peep_id: 1, message: 'Test peep', timestamp: Date.today )}
+  let(:peep) { double(:peep, peep_id: 1, message: 'Test peep', timestamp: '2021-07-19')}
   context '#initialize' do
     it 'should initialize with set attributes' do
       expect(peep.peep_id).to eq(1)
       expect(peep.message).to eq('Test peep')
-      expect(peep.timestamp).to eq("#{Date.today}")
+      expect(peep.timestamp).to eq('2021-07-19')
     end
   end
 end
