d123a1fc56f5d8fdc275663e5a17a7591ff77b14
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-21 22:30:37 +0100

    failing test - feature test added to test to see a peeps can be created, seperated from view all feature test

--- spec/features/create_peeps_spec.rb
+++ spec/features/create_peeps_spec.rb
@@ -1,10 +1,11 @@
-# frozen_string_literal: true
 require_relative '../unit/web_helper'
 
 feature 'Can view peeps' do
   scenario 'user can view peeps in the feed' do
     signup
     login
+    fill_in('message', with: '1st Test Peep!')
+    click_button('Post a Peep')
     expect(page).to have_content("1st Test Peep!")
   end
 end
