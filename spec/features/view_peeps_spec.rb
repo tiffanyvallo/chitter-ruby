e4d5f76d3b47322be211e8b152fc4406fbde2efe
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-21 22:29:04 +0100

    Failing test acreated feature test for .create peeps

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -5,6 +5,8 @@ feature 'Can view peeps' do
   scenario 'user can view peeps in the feed' do
     signup
     login
+    fill_in('message', with: '1st Test Peep!')
+    click_button('Post a Peep')
     expect(page).to have_content("1st Test Peep!")
   end
 end
